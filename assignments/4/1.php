aabfc6e152fd35730a363929d79ed57c8adef6a3
Lucky Kispotta <luckykispotta112@gmail.com>
2022-03-11 01:17:38 +0530

    test

--- assignments/4/1.php
+++ assignments/4/1.php
@@ -1,8 +1,22 @@
 <?php
 function question() {
 global $N, $M;
+Table $tb = new Table;
+$da = 3/100 * $N;
+$hra = 15/100 * $N;
+$pf = 12/100 * $N;
+$gross = $N + $da +$hra + $pf;
+$net = $gross - $pf;
+$tb->addHeadRow(["Type", "Amount"]);
+$tb->addRow(["Dearness Allowance", $da]);
+$tb->addRow(["House Rental Allowance", $hra]);
+$tb->addRow(["Provident Fund", $pf]);
+$tb->addRow(["Gross Salary", $gross]);
+$tb->addRow(["Net Salary", $net]);
 
-echo "NAME : $N <br> Basic Salary : $M";
+echo "NAME : $N <br> Basic Salary : $M <br>";
+
+echo $tb->getTable();
 }
 question();
 ?>
