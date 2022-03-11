9f957a77ae607cc4a1d988ca246c3dd9684b5bff
Lucky Kispotta <luckykispotta112@gmail.com>
2022-03-11 11:33:47 +0530

    fixed wrong ajax requests

--- js/ajax.js
+++ js/ajax.js
@@ -3,10 +3,10 @@ function work(link) {
 }
 const ajax = (link, resolve) => {
   const aj = new XMLHttpRequest();
+  aj.open("GET", link);
   aj.setRequestHeader("Cache-Control", "no-cache, no-store, must-revalidate");
   aj.setRequestHeader("Pragma", "no-cache");
   aj.setRequestHeader("Expires", "0");
-  aj.open("GET", link);
   aj.send();
   aj.onreadystatechange = () => {
     if (aj.status == 200 && aj.readyState == 4) resolve(aj.responseText);
